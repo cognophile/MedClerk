00e4e6b0a6fecbe3ccf1eb85159ae65ee510e825
Cognophile <cognophile@users.noreply.github.com>
2017-10-20 21:06:11 +0100

    Add NUnit and package restore config

---
+++ MedClerk/MedClerk/Tests/ExampleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void exampleTest_firstTestCaseTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void exampleTest_secondTestCaseTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
