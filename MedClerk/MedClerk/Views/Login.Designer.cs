86edb3da5deb5209f0439ae69481ae669b18ec11
MSpen117 <32450276+MSpen117@users.noreply.github.com>
2017-10-23 10:44:10 +0100

    Changes to default form name
    From form 1 to Login

--- MedClerk/MedClerk/Views/Login.Designer.cs
+++ MedClerk/MedClerk/Views/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MedClerk
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1475, 700);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MedClerk/MedClerk/Views/Login.cs
+++ MedClerk/MedClerk/Views/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MedClerk
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
