494c13b1b6ff000aaaea8d65d4053e0ae61b7546
Cognophile <cognophile@users.noreply.github.com>
2017-10-24 21:39:11 +0100

    Add password hashing and tests

---
+++ MedClerk/MedClerk/Tests/PasswordTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedClerk.Utilities;
+using NUnit.Framework;
+
+namespace MedClerk.Tests
+{
+    class PasswordTest
+    {
+        [Test]
+        public void TestPasswordHashing_ReturnsExpected_Md5HashValue()
+        {
+            // Arrange
+            var password = "password";
+            var expected = "5f4dcc3b5aa765d61d8327deb882cf99";
+
+            // Act
+            var passwordHash = Password.Hash(password);
+
+            // Assert
+            Assert.That(passwordHash, Is.EqualTo(expected));
+        }
+    }
+}
