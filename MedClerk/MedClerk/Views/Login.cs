7c694c682e653ea0dd95fc00ae9057911b552616
Cognophile <cognophile@users.noreply.github.com>
2017-10-24 23:34:54 +0100

    Add login event to call upon controller login

--- MedClerk/MedClerk/Views/Login.cs
+++ MedClerk/MedClerk/Views/Login.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MedClerk.Controllers;
+using MedClerk.Views;
 
 namespace MedClerk
 {
@@ -24,6 +26,27 @@ namespace MedClerk
 
         private void enterButton_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(usernameTextBox.Text) || String.IsNullOrEmpty(passwordTextBox.Text))
+            {
+                MessageBox.Show("You must enter credentials into both fields!", "Missing required fields",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                usernameTextBox.Clear();
+                passwordTextBox.Clear();
+                return;
+            }
+
+            if (LoginController.Login(usernameTextBox.Text, passwordTextBox.Text))
+            {
+                Views.MainMenu menu = new Views.MainMenu();
+                this.Hide();
+                menu.Closed += (senderObj, eventArgs) => this.Close();
+                menu.Show();
+            } else {
+                MessageBox.Show("The provided credentials cannot be verified. Please try again.", "Invalid Login!", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                usernameTextBox.Clear();
+                passwordTextBox.Clear();
+            } 
         }
     }
 }
