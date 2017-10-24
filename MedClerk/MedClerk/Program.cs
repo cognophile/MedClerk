4a645e5d8827672fe86c09e04d30034c645599b5
Cognophile <cognophile@users.noreply.github.com>
2017-10-24 23:51:03 +0100

    Update to new Login view namespace

--- MedClerk/MedClerk/Program.cs
+++ MedClerk/MedClerk/Program.cs
@@ -16,7 +16,7 @@ namespace MedClerk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Views.Login());
         }
     }
 }
