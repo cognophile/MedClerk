93473a2a89f15bfe0c09204b395412b78885ae00
Cognophile <cognophile@users.noreply.github.com>
2017-10-24 23:33:49 +0100

    Add controller to mediate view with password verification

---
+++ MedClerk/MedClerk/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedClerk.Utilities;
+
+namespace MedClerk.Controllers
+{
+    class LoginController
+    {
+        /// <summary>
+        /// Login the user identified by the given credentials
+        /// </summary>
+        /// <param name="username">User account username string</param>
+        /// <param name="password">User plain-text password string</param>
+        /// <returns>True if match found, else false</returns>
+        public static Boolean Login(string username, string password)
+        {
+            var passwordHash = Password.Hash(password);
+            bool isVerified = Password.Verify(username, passwordHash);
+            return isVerified ? true : false;
+        }
+    }
+}
