9292e2a0ad53e50258133fd99fd48fb84e245141
Cognophile <cognophile@users.noreply.github.com>
2017-10-25 19:35:55 +0100

    Fix missing using directives

--- MedClerk/MedClerk/Models/UserModel.cs
+++ MedClerk/MedClerk/Models/UserModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MedClerk.Database;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
