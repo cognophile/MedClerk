4edbf709c511f51d25112f84d97105a8ab75e6fc
Cognophile <cognophile@users.noreply.github.com>
2017-11-06 17:46:39 +0000

    Add Date/Time helper methods and their unit tests

---
+++ MedClerk/MedClerk/Tests/DateTimeFormatterTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MedClerk.Utilities;
+using System.Globalization;
+
+namespace MedClerk.Tests
+{
+    class DateTimeFormatterTest
+    {
+        [Test]
+        public void TestConverToUkFormat_GivenADefaultUsFormat_ReturnsExpectedUkDateFormatString()
+        {
+            // Arrange
+            var expected = "5/12/2017";
+            DateTime testDate = new DateTime(2017, 12, 5);
+            var testDateStr = testDate.Date.ToString("d");
+            
+            // Act
+            var date = DateTimeFormatter.ConvertToUkFormat(testDateStr);
+
+            // Assert
+            Assert.That(date, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestExcludeSeconds_GivenADefaultTimeFormatString_ReturnsExpectedTimeStringWithoutSeconds()
+        {
+            // Arrange
+            var expected = "12:30";
+            var initialTime = "12:30:05";
+
+            // Act
+            var time = DateTimeFormatter.RemoveSeconds(initialTime);
+
+            // Assert
+            Assert.That(time, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ MedClerk/MedClerk/Utilities/DateTimeFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedClerk.Utilities
+{
+    /// <summary>
+    /// Provides formatting manipulations using 'DateTime' functions.
+    /// </summary>
+    class DateTimeFormatter
+    {
+        /// <summary>
+        /// Function to convert from a US format date (MM/dd/yyyy) to UK format (dd/MM/yyyy)
+        /// </summary>
+        /// <param name="dateUsFormat">String representation of a date, in US format 'MM/dd/yyyy', without the time appended.</param>
+        /// <returns>String representing the UK format of the given Date</returns>
+        public static string ConvertToUkFormat(string dateUsFormat)
+        {
+            DateTime dt = DateTime.ParseExact(dateUsFormat, "MM/d/yyyy", CultureInfo.InvariantCulture);
+            return dt.ToString("d/MM/yyyy");
+        }
+
+        /// <summary>
+        /// Function to remove the seconds from a Time string
+        /// </summary>
+        /// <param name="time">A string represenation of the time in "HH:mm:ss" format.</param>
+        /// <returns>String representation of the time without the seconds appended. Eg. "HH:mm".</returns>
+        public static string RemoveSeconds(string time)
+        {
+            return DateTime.Parse(time.ToString(), CultureInfo.InvariantCulture).ToString("HH:mm");
+        }
+    }
+}
